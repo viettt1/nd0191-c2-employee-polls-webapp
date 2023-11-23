4a9108119e44bdef3d9df96b41962b09456dac42
Viet Tran <viet.tuantran@coxautoinc.com>
2023-11-23 20:53:41 +0700

    update test

--- src/util/_DATA.test.js
+++ src/util/_DATA.test.js
@@ -1,4 +1,4 @@
-const { _saveQuestionAnswer } = require("./_DATA");
+const { _saveQuestionAnswer, _saveQuestion } = require("./_DATA");
 describe("_saveQuestionAnswer", () => {
     it("Should return true with correct User", async () => {
         const response = await _saveQuestionAnswer({
@@ -18,5 +18,31 @@ describe("_saveQuestionAnswer", () => {
         }).catch(e => e);
 
         expect(response).toBe("Please provide authedUser, qid, and answer");
+    });
+});
+
+describe("_saveQuestion", () => {
+    it("Should return formated question with correct author", async () => {
+        setTimeout(async () => {
+            const response = await _saveQuestion({
+                optionOneText: 'option one test',
+                optionTwoText: 'option two test',
+                author: {
+                    id: 'tylermcginnis'
+                }
+            })
+
+            expect(response.author).toBe('tylermcginnis')
+        }, 1000)
+    });
+
+    it("Should return error with invalid User", async () => {
+        const response = await _saveQuestion({
+            optionOneText: 'option one test',
+            optionTwoText: 'option two test',
+            author: undefined
+        }).catch(e => e);
+
+        expect(response).toBe("Please provide optionOneText, optionTwoText, and author");
     });
 });
